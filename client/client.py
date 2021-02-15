2b5008548334016a4f3139dd566ff20ebbcb0f49
brat_volk <43145630+brat-volk@users.noreply.github.com>
2021-02-15 19:27:20 +0100

    Update client.py

--- client/client.py
+++ client/client.py
@@ -9,8 +9,8 @@ host = victim_IP
 port = input('port: ')
 addr = (victim_IP, port)
 socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if(socket1.connect(addr)):
-    print "Connected"
+if(socket1.connect(addr)): #passed type string to int var FUCCCCCCCCCCCCCCC
+    print("Connected")
 text_finished = "Disconnect"
 file_text = ""
 while file_text != text_finished:
